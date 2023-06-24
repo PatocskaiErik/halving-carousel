f1d503f8f7b92860e88f2fb4d325957c0996a850
Erik <patoerik93@gmail.com>
2023-06-24 13:31:58 +0200

    Added calculations

--- src/main/java/com/epam/rd/autotasks/HalvingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/HalvingCarousel.java
@@ -1,9 +1,17 @@
 package com.epam.rd.autotasks;
 
 public class HalvingCarousel extends DecrementingCarousel {
-
     public HalvingCarousel(final int capacity) {
-        super(capacity);
-    }
 
+        super(capacity);
+
+    }
+    @Override
+    public CarouselRun run() {
+        if (!isRun) {
+            isRun = true;
+            return new HalvingCarouselRun();
+        }
+        return null;
+    }
 }
